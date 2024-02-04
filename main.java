f62f5d9e7ecb445b45fe7df2ec7f4d7cfbacb842
Pranjal039 <pranjal.h@gmail.com>
2024-02-05 01:06:04 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+ class Main {
+    public static void main(String[] args){
+                   System.out.println("This is a java program");
+                System.out.println("output from java program");
+                 System.out.println(new Date());
+
+    }
+}
